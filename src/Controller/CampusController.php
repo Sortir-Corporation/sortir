93e4330b66ed6d23238da02d6484105742bf2d0d
chaer4real <shaq.63@hotmail.fr>
2026-05-20 16:28:33 +0200

    feat: Campus et register

---
+++ src/Controller/CampusController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Campus;
+use App\Form\CampusType;
+use App\Repository\CampusRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('campus', name: 'campus_')]
+final class CampusController extends AbstractController
+{
+    #[Route('', name: 'campus_list', methods: ['GET'])]
+    public function list(CampusRepository $campusRepository): Response
+    {
+        return $this->render('campus/list.html.twig', [
+            'campus' => $campusRepository->findAll(),
+        ]);
+    }
+    #[Route('/create', name: 'create', methods: ['GET', 'POST'])]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $campus = new Campus();
+        $form = $this->createForm(CampusType::class, $campus);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($campus);
+            $em->flush();
+
+            $this->addFlash('success', 'Campus créé avec succès');
+            return $this->redirectToRoute('campus_campus_list');
+        }
+
+        return $this->render('campus/new.html.twig', [
+            'form' => $form->createView(),
+            'title' => 'Créer un campus'
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(Campus $campus, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(CampusType::class, $campus);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            $this->addFlash('success', 'Campus modifié avec succès');
+            return $this->redirectToRoute('campus_campus_list');
+        }
+
+        return $this->render('campus/edit.html.twig', [
+            'form' => $form->createView(),
+            'campus' => $campus,
+        ]);
+    }
+
+    #[Route('/{id}/delete', name: 'delete', methods: ['POST'])]
+    public function delete(Campus $campus, EntityManagerInterface $em): Response
+    {
+            $em->remove($campus);
+            $em->flush();
+            $this->addFlash('success', 'Campus supprimé');
+
+        return $this->redirectToRoute('campus_campus_list');
+    }
+}
